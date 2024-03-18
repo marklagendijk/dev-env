137e343492be45dadc9284b43bef441166f4e665
Mark Lagendijk <mark@lagendijk.nl>
2024-03-18 09:04:47 +0100

    Create windows-tools.ps1

---
+++ windows-tools.ps1
@@ -0,0 +1,41 @@
+# Work
+winget install Microsoft.VisualStudio.2022.BuildTools
+winget install OpenJS.NodeJS.LTS
+winget install dbeaver.dbeaver
+winget install LINQPad.LINQPad.8
+winget install JetBrains.Toolbox
+winget install Microsoft.WindowsTerminal
+winget install gerardog.gsudo
+winget install Microsoft.AzureCLI
+winget install Git.Git
+winget install HTTPie.HTTPie
+winget install Microsoft.Teams
+winget install Microsoft.VisualStudioCode
+dotnet tool install --global microsoft.sqlpackage
+dotnet tool install --global dotnet-ef
+
+
+# Browsers
+winget install Google.Chrome
+winget install Microsoft.Edge
+winget install Mozilla.Firefox
+
+# Other
+winget install 7zip.7zip
+winget install Docker.DockerDesktop
+winget install OpenWhisperSystems.Signal
+winget install Dropbox.Dropbox
+winget install Foxit.FoxitReader
+winget install GIMP.GIMP
+winget install Greenshot.Greenshot
+winget install Logseq.Logseq
+winget install OBSProject.OBSStudio
+winget install RoyShilkrot.obs-backgroundremoval
+winget install JanDeDobbeleer.OhMyPosh
+winget install TeamViewer.TeamViewer
+winget install VideoLAN.VLC
+winget install WinSCP.WinSCP
+winget install BrianApps.Sizer.Dev
+winget install Microsoft.PowerToys
+winget install Famatech.AdvancedIPScanner
+winget install TheDocumentFoundation.LibreOffice
